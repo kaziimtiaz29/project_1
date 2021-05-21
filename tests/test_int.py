d27814cf3235cf7c3481164bb02a90a5285db48b
Kazi Uddin <uddinimtiaz30@gmail.com>
2021-05-21 14:38:21 +0100

    project1

---
+++ tests/test_int.py
@@ -0,0 +1,35 @@
+from flask.helpers import url_for
+from selenium import webdriver
+from flask_testing import LiveServerTestCase
+from application import app, db
+from urllib.request import urlopen
+class TestBase(LiveServerTestCase):
+    def create_app(self):
+        app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db" # change to a test sqlite database
+        app.config['LIVESERVER_PORT'] =5050
+        app.config['SECRET_KEY'] = "1234"
+        app.config['DEBUG']= True
+        app.config['TESTING']= True
+        return app
+
+    def setUp(self):
+        chrome_options = webdriver.chrome.options.Options()
+        chrome_options.add_argument('--headless') # must be headless
+
+        self.driver = webdriver.Chrome(options=chrome_options) 
+
+        db.create_all() # create schema before we try to get the page
+        self.driver.get(f'http://localhost:5050/')
+
+    def tearDown(self):
+        self.driver.quit()
+        db.drop_all()
+    def test_server_is_up_to_and_running(self):
+        response=urlopen("http://localhost:5050/")
+        self.assertEqual(response.code, 200)
+    
+class Testviews(TestBase):
+        def test_navigation(self):
+           self.driver.find_element_by_xpath ("/html/body/a[2]").click()
+           self.assertIn(url_for("to_add"), self.driver.current_url)
+        
